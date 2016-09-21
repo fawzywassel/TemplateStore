a6ab56e1bc6daa0ad815995fe2692493ac52e9b2
fawzywassel <fawzywassel@gmail.com>
2016-09-21 18:04:34 +0300

    first ver

---
+++ angular-seed-master/app/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+// Declare app level module which depends on views, and components
+angular.module('templateStore', [
+  'ngRoute',
+  'myApp.view1',
+  'myApp.view2',
+  'templateStore.templates'
+]).
+config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
+  $locationProvider.hashPrefix('!');
+
+  $routeProvider.otherwise({redirectTo: '/templates'});
+}]);
